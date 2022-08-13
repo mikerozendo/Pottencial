ccd84b97bb3be0171920cb7491749beec66c19a2
mikerozendo <michaelrozendo@ampm-info.com.br>
2022-08-13 15:04:03 -0300

    criando base controller p/ abstrair logica de injecoes com service provider

---
+++ src/Pottencial/Pottencial.Presentation.API/Controllers/AppBaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pottencial.Presentation.API.Controllers
+{
+    public class AppBaseController : ControllerBase
+    {
+        protected readonly IServiceProvider _serviceProvider;
+        public AppBaseController(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        protected object GetService(Type serviceType)
+        {
+            return _serviceProvider.GetService(serviceType) ?? throw new ArgumentNullException("Erro interno ao requisitar serviços de aplicação");
+        }
+    }
+}
