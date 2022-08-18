385c1a53252bc5a62a2ef87a204635c59d9a4cdb
mikerozendo <mikerozendo@gmail.com>
2022-08-18 16:27:19 -0300

    adicionando primeiro value object do projeto

--- src/Pottencial/Pottencial.Domain/Entities/Pessoa.cs
+++ src/Pottencial/Pottencial.Domain/Entities/Pessoa.cs
@@ -1,7 +1,10 @@
-﻿namespace Pottencial.Domain.Entities;
+﻿using Pottencial.Domain.ValueObjects;
+
+namespace Pottencial.Domain.Entities;
 
 public class Pessoa : Base
 {
     public string Nome { get; set; }
     public string Sobrenome { get; set; }
+    public CPF CPF { get; set; }
 }
--- src/Pottencial/Pottencial.Domain/ValueObjects/CPF.cs
+++ src/Pottencial/Pottencial.Domain/ValueObjects/CPF.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pottencial.Domain.Entities
+namespace Pottencial.Domain.ValueObjects
 {
-    public class Vendedor : Pessoa
+    public class CPF
     {
-
     }
 }
