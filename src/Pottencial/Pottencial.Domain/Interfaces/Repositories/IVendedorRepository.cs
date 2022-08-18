cc7ff5286afd5118f159a330517b6e5f88258b83
mikerozendo <mikerozendo@gmail.com>
2022-08-18 17:00:54 -0300

    adicionando interface + classe p/ repository de vendedor

---
+++ src/Pottencial/Pottencial.Domain/Interfaces/Repositories/IVendedorRepository.cs
@@ -0,0 +1,5 @@
+﻿namespace Pottencial.Domain.Interfaces.Repositories;
+
+public interface IVendedorRepository
+{
+}
---
+++ src/Pottencial/Pottencial.Infraestructure.Data/Repositories/VendedorRepository.cs
@@ -0,0 +1,8 @@
+﻿using Pottencial.Domain.Interfaces.Repositories;
+
+namespace Pottencial.Infraestructure.Data.Repositories
+{
+    public class VendedorRepository : IVendedorRepository
+    {
+    }
+}
