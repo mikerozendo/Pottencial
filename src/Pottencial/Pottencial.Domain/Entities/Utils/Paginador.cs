e134787b9694659a46cc2eee108a0e3bbc2d5612
mikerozendo <mikerozendo@gmail.com>
2022-08-19 20:52:52 -0300

    implementando utilitario do paginador

---
+++ src/Pottencial/Pottencial.Domain/Entities/Utils/Paginador.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace Pottencial.Domain.Entities.Utils;
+
+public class Paginador<T>
+{
+    public const int QuantidadeMaximaItens = 50;
+
+    public int ObterQuantidadePaginas(IEnumerable<T> listagem)
+    {
+        if (listagem.Count() <= 50) return 1;
+
+        int quantiadeItens = ObterQuantidadeItens(listagem);
+        decimal quociente = ObterQuocientePaginas(quantiadeItens, QuantidadeMaximaItens);
+
+        bool decimalMaiorQZero = int.TryParse(ObterValorDecimal(quociente), out int value);
+
+        return decimalMaiorQZero ? ((int)(quociente + 1)) : ((int)quociente);
+    }
+
+    private int ObterQuantidadeItens(IEnumerable<T> listagem)
+    {
+        return listagem.ToList().Count;
+    }
+
+    private decimal ObterQuocientePaginas(int quantidadeItens, int quantidadeMaximItens)
+    {
+        return (decimal)quantidadeItens / (decimal)quantidadeMaximItens;
+    }
+
+    private string ObterValorDecimal(decimal quociente)
+    {
+        return quociente.ToString("F2", CultureInfo.InvariantCulture).Split('.')[1];
+    }
+}
---
+++ src/Pottencial/Pottencial.Test/Pottencial.Domain.Test/PaginadorTeste.cs
@@ -0,0 +1,48 @@
+﻿using Pottencial.Domain.Entities.Utils;
+using Pottencial.Test.Pottencial.Domain.Test.Utils;
+
+namespace Pottencial.Test.Pottencial.Domain.Test;
+
+public class PaginadorTeste
+{
+    [Fact]
+    public void TestaQuantidadePaginas()
+    {
+        //Arrange
+        var list = new PaginadorHelper().ObterMock();
+        var paginador = new Paginador<string>();
+        
+        //Act
+        int paginas = paginador.ObterQuantidadePaginas(list);
+
+        //Assert
+        Assert.Equal(3, paginas);
+    }
+
+    [Fact]
+    public void TestaQuantidadePaginas_Colecao_Zerada()
+    {
+        //Arrange
+        var paginador = new Paginador<string>();
+
+        //Act
+        int paginas = paginador.ObterQuantidadePaginas(new List<string>());
+
+        //Assert
+        Assert.Equal(1, paginas);
+    }
+
+    [Fact]
+    public void TestaQuantidadePaginas_Menor()
+    {
+        //Arrange
+        var paginador = new Paginador<string>();
+        var list = new PaginadorHelper().ObterMockMenor50();
+
+        //Act
+        int paginas = paginador.ObterQuantidadePaginas(list);
+
+        //Assert
+        Assert.Equal(1, paginas);
+    }   
+}
